876c064de8593a86a625cd4565e62cf8787d678b
Mohammad <its_mh@outlook.com>
2020-10-13 19:28:42 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Weather from './Components/Weather/Weather';
+import { Route, Link } from "react-router-dom";
+import About from './Components/About'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Link to='/'>
+        <h2 className='Logo'>The<br/> Weather<br/> App</h2>
+      </Link>
+      <Route exact path='/' component={Weather}/>
+      <Link className='aboutLink' to='/About'>
+      <footer><h1>About</h1></footer>
+        </Link>
+
+      
+      <Route exact path='/About' component={About}/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About (){
+    return(
+        <div className='aboutSection'>
+    <h1>About Us</h1>
+    <p>Welcome to our weather app. <br/>The weather app that is designed around you, <br/>the user.</p>
+    </div>
+    )
+}
+
+
+export default About;
